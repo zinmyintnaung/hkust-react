73b840f657c51900c9c1c81726863af152d05934
zinmyintnaung <zinmyintnaung@gmail.com>
2021-09-23 14:36:46 +0800

    form validation

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
-
-
 function About(props) {
 
     function RenderLeader ({leader}) {
